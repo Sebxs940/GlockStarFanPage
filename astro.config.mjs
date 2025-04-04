66b2061d8bbf876668194377ad9dc3ebd5e1a90b
Sebxs940 <sprogramacion940@gmail.com>
2025-04-04 17:35:17 -0500

    Fort Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  site: 'https://tu-dominio-en-render.onrender.com', // Actualiza esto con tu dominio de Render
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
