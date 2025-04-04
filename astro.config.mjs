80f4e9cb40ea02247659092201d5a349280426ad
Sebxs940 <sprogramacion940@gmail.com>
2025-04-04 17:32:04 -0500

    Third Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static'
+  output: 'static',
+  site: 'https://tu-dominio-en-render.onrender.com', // Actualiza esto con tu dominio de Render
 });
