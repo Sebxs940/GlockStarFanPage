f5cc25cef1154bd0b58fd8d6bf4f49c814dc64e3
Sebxs940 <sprogramacion940@gmail.com>
2025-04-04 17:21:12 -0500

    Second Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import staticAdapter from '@astrojs/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  adapter: staticAdapter()
+  output: 'static'
 });
