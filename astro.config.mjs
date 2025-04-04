7e3c3d0ed437d9bda36e5ec3a9ab7b2cefb87ac4
Sebxs940 <sprogramacion940@gmail.com>
2025-04-04 12:37:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import staticAdapter from '@astrojs/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'static',
+  adapter: staticAdapter()
 });
